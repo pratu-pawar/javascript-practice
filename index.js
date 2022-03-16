18d4891db56e9075a9ba4ad1c1791395027fab3a
pratu-pawar <pratikshapawar3498@gmail.com>
2022-03-16 08:20:00 +0530

    experation and operators

---
+++ index.js
@@ -0,0 +1,45 @@
+// console.log("hello world");
+
+// // values and veriables in javascript //
+
+// var myName = 'pratiksha pawar';
+// console.log (myName);
+
+// var myAge = 23
+// console.log(myAge);
+
+// // challange time //
+// var _myName = "vinod";
+
+// console.log(_myName );
+
+// var _1my_ _Name = "bhadur";
+// console.log(_1my_ _Name );
+
+// // DATA TYPE IN JAVASCRIPT //
+// 
+// var myName = "vinod thapa";      //string //
+// console.log(myName);
+// var myAge = 26;
+// console.log(typeof(myAge));      //number//
+
+// var iAmpawar = true;
+// console.log(iAmpawar);
+// console.log(typeof(iAmpawar));  // boolen//
+
+// EXPRESSION AND OPERATORS //
+//ASSIGNMENT OPERATOES //
+// var x = 5;
+// var y = 5;
+// console.log(x == y);
+
+//ARITHMETIC OPRATORS//
+// console.log(3+3);
+// console.log(10-5);
+// console.log(20/5);
+// console.log(5+6);
+console.log("remainder operator" + 27%4);
+
+
+
+
