c57e51d0c085dc5817ba4c7ebabdc01d9f65f25c
pratu-pawar <pratikshapawar3498@gmail.com>
2022-03-17 14:25:25 +0530

    if..else statment

---
+++ 16_march.js
@@ -0,0 +1,95 @@
+//INCRIMENT AND DECREMENT OPERATORS //
+// var num = 15;
+// var newNum = num++;
+// console.log(vnum);
+// console.log(newNum );
+
+//postfix//
+
+// var num = 15;
+// var newNum = num++ + 5;
+// console.log(num);
+// console.log(newNum);
+
+
+// //prefix//
+// var num = 15;
+// var newNum =++num + 5;
+// console.log(num);
+// console.log(newNum);
+
+//decrement//
+// var num = 15;
+// var newNum = num-- + 5;
+// console.log(num);
+// console.log(newNum);
+
+// var num = 15;
+// var newNum = --num + 5;
+// console.log(num);
+// console.log(newNum);
+
+//COMPARISION OPERATOES//
+var a = 30;
+var b = 10;
+//equal (==)
+// console.log(a === b);
+
+// not equql to //
+// console.log(a != b);
+
+// //
+//greater than (>)//
+// console.log(a > b);
+// console.log(b > a);
+
+//greater than or equal//
+// console.log(a >= b);
+// console.log(b >= a);
+
+//less than //
+// console.log(a < b);
+// console.log(b < a);
+
+// less than or equal //
+// console.log(a <= b);
+// console.log(b <= a);
+
+// LOGICAL OPERATOES //
+// var a = 30 ;
+// var b = -20;
+// //logical AND (&&) //
+
+// 
+// console.log(a > b && b > -50 && b < 0);
+
+//logical (OR ) //
+// console.log((a > b) || (b > 0) || (b < 0));
+
+// logical not (!) //
+// console.log(!(a>0) || (b<0));
+// console.log (!false);
+// console.log(! true);
+
+// STRING OPERATOR (CONCATENATION) //
+// console.log("hello world");
+// console.log("hello" + "world");
+// console.log("pratu" + "suraj");
+
+//CONTROL STATMENT & LOOP //
+// if .. else //
+// if raning = raincoat 
+// else no raincoat
+// var tomr = "rain";
+// if (tomr == "rain"){ 
+//     console.log("take a raincoat");
+
+// }else{ 
+//     console.log("dont take a  raincoat");
+// }
+
+
+
+
+    
+
