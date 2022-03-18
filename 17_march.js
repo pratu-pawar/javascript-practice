433bdd64d46d0f9c31e5aae2f2273c56b5533435
pratu-pawar <pratikshapawar3498@gmail.com>
2022-03-18 10:38:38 +0530

    functions

---
+++ 17_march.js
@@ -0,0 +1,125 @@
+//CONDITIONAL (TERNARY) OPERATOR //
+// var age = 17;
+// (age >= 18) ? "you can vote" : "you cant vote";
+// console.log((age >= 18) ? "you can vote" : "you cant vote");
+
+// var age = 23;
+// (age >= 18) ? "you can vote" : "you cant vote";
+// console.log((age >= 18) ? "you can vote" : "you cant vote");
+
+//SWITCH STATMENT // 
+// var area  = 'circle';
+// var PI = 3.142, l=5,r=3 ;
+// switch (area){ 
+//     case "circle" :
+//         console.log("the area of the circle is : " + PI*r**2);
+       
+//     case 'triangle' :
+//         console.log("the area of the triangle is : " + (l*b/2)); 
+//         break ;
+    
+//     case 'recatangle':
+//         console.log("the area of the rectangle is : " + (l*b));   
+//         break ; 
+//    default :
+//        console.log("please enter valid data");
+
+// }
+// while loop statment // //block scope//
+// var num = 0;
+// while (num <= 1000){ 
+//    console.log(num);    // infinite loop//
+//    num++;
+// } 
+// do- while loop //
+// var num = 0;
+// do{ 
+//     console.log(num); // infinite loop//
+//     num++
+// } while (num<= 10);
+
+//for - loop//
+// var num = 0 ;
+// for(var num = 0; num <= 10; num++){ 
+//     console.log(num);
+// }
+// var num = 20;
+// for(var num = 20; num <=30; num++){ 
+//     console.log(num);
+// }
+
+
+//javascript program to print table for given no (8,9,20,30) by using the for loop//
+// for(var num = 1; num <= 10; num++){ 
+//     var tableof = 8;
+//     console.log(tableof + " * " + num + " = " + tableof * num);
+// }
+
+// for(var num = 1; num <= 10; num++){ 
+//     tableof = 9;
+//     console.log(tableof + " * " + num + " = " + tableof * num);
+// } 
+
+// for(var num = 1; num <= 10; num++){ 
+//     tableof = 20;
+//     console.log(tableof + " * " + num + " = " + tableof * num);
+// } 
+
+// for(var num = 1; num <= 10; num++){ 
+//     tableof = 30;
+//     console.log(tableof + " * " + num + " = " + tableof * num);
+// } 
+
+//FUNCTION JAVASCRIPT //
+
+// function defination //
+// var a = 20;
+// var b = 30;
+// function sum(){ 
+//     var total = a+b;
+//     console.log(total);
+// }
+// // calling function//
+// sum();
+
+// function parameter vs arguments
+// function sum (a , b){ 
+//     var total = a + b;
+//     console.log(total);
+// }                                  //parameter is a and b //    
+// sum();
+// sum(20,30);                          //argument is 20 and 30 mens numbers//
+// sum(50,50)
+
+
+//function expression //
+// function sum(a,b){ 
+//     var total = a+b;
+//     console.log(total);
+
+// }
+// var funExp = sum(60,49);
+
+//return keybords//
+// function sum(a,b ){ 
+//     return total = a+b;
+
+// }
+// var funExp =sum(36,67);
+// console.log('the sum of the no is ' + funExp);
+
+
+//anonymous function//
+// var funExp = function(a,b){ 
+//     return total = a+b;
+    
+
+// }
+// var sum = funExp(10,23);
+// console.log('the sum of two no is' +sum);
+
+
+
+
+
+
