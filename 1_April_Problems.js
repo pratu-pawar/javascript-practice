dac4d7ef29261da3ce9517418828bd65ef7a7287
Spawar1530 <spawar1530@gmail.com>
2022-04-01 10:42:08 +0530

    1_april problems

---
+++ 1_April_Problems.js
@@ -0,0 +1,63 @@
+// Question 1 :
+//Create a function that takes two numbers as arguments and returns their sum.
+//For example if you write a function "addition", then "addition(2, 4)" should return 6
+//Please write you answer below:
+
+
+
+
+
+
+// Question 2 :
+//Create a function that takes an array containing only numbers and return the first element.
+//For example if you write a function "getFirstValue", then "getFirstValue([1, 2, 3])" should return 1
+//Please write you answer below:
+
+
+
+
+
+
+// Question 3 :
+//Write a function that converts hours into seconds.
+//For example if you write a function "howManySeconds", then "howManySeconds(2)" should return 7200 (here "2" is hours)
+//Please write you answer below:
+
+
+
+
+
+
+
+// Question 4 :
+//Create a function that takes length and width and finds the perimeter of a rectangle.
+//For example if you write a function "findPerimeter", then "findPerimeter(6, 7)" should return 26. Formula for finding perimeter is 2 * (length + width)
+//Please write you answer below:
+
+
+
+
+
+
+
+
+// Question 5 :
+//Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+//For example if you write a function "lessThan100", then "lessThan100(22, 15)" should return true as addition of 22 and 15 is 37 which is less than 100
+//Please write you answer below:
+
+
+
+
+
+
+
+
+
+// Question 6 :
+// Write the use of "&&" operator and "||" operator, write in comments 
+//Please write you answer below:
+
+
+
+
