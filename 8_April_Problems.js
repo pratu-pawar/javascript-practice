97d77ba2e8650e048e109fc6687a0bf8cc6a15ba
Spawar1530 <spawar1530@gmail.com>
2022-04-08 10:38:38 +0530

    8 april problems

---
+++ 8_April_Problems.js
@@ -0,0 +1,28 @@
+//Que 1: Create a function that returns true when num1 is equal to num2; otherwise return false
+//Examples: isSameNum(4, 8) ➞ false, isSameNum(2, 2) ➞  true
+
+//Que 2: Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 10
+//Examples: makesTen(9, 10) ➞ true, makesTen(9, 9) ➞ false, makesTen(1, 9) ➞ true
+
+//Que 3: A bartender is writing a simple program to determine whether he should serve drinks to someone. 
+//He only serves drinks to "people 18" and older and when "he's not on break".
+//Examples: shouldServeDrinks(17, true) ➞ false, shouldServeDrinks(19, false) ➞ true, shouldServeDrinks(30, true) ➞ false
+//as shown in above examples, first argument is age of person and second argument determines if bartender is present or not,
+//true means present and false means he is on break
+
+//Que 4: Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers
+//Examples: isEvenOrOdd(3) ➞ "odd", isEvenOrOdd(146) ➞ "even"
+
+//Que 5: Luke Skywalker has family and friends. 
+//Help him remind them who is who. Given a string with a name, return the relation of that person to Luke.
+
+//Person	    Relation
+//Darth Vader	father
+//Leia	        sister
+//Han	        brother in law
+//R2D2	        droid
+
+//In above table relation of each person with luke is given, consider the example: 
+//relationToLuke("Darth Vader") ➞ "Luke, I am your father."
+//In example you can see Darth Vader is father of luke hence we returned the "Luke, I am your father."
+//If person passed to function id R2D2 then output will be "Luke, I am your droid."
