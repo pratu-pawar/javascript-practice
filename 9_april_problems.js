d87f54a11fe09140ee620cb82e75ff37c4d42d3b
Spawar1530 <spawar1530@gmail.com>
2022-04-09 11:22:43 +0530

    9 april problems

---
+++ 9_april_problems.js
@@ -0,0 +1,19 @@
+//Que 1: A student learning JavaScript was trying to make a function. 
+//His code should concatenate a passed string name with string "Edabit" and store it in a variable called result. 
+//Examples: nameString("Mubashir") ➞ "MubashirEdabit", nameString("Matt") ➞ "MattEdabit"
+
+//Que 2: Create a function that returns the number of arguments it was called with
+//Examples: numArgs() ➞ 0, numArgs("foo") ➞ 1
+
+//Que 3: Create a function that takes a string of lowercase characters and returns that string reversed and in upper case
+//Examples: reverseCapitalize("abc") ➞ "CBA", reverseCapitalize("hellothere") ➞ "EREHTOLLEH"
+
+//Que 4: You are given an array of integers having both negative and positive values, except for one integer which can be negative or positive. 
+//Create a function to find out that integer.
+//Examples: lonelyInteger([1, -1, 2, -2, 3]) ➞ 3
+// 3 has no matching negative appearance.
+
+//Que 5: A palindrome is a word that is identical forward and backwards.
+//mom, racecar, kayak -> palidromes
+//create a function that checks whether it is a palindrome
+//Examples: checkPalindrome("mom") ➞ true, checkPalindrome("scary") ➞ false
