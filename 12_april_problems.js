bf8754b408291fb1ec511fa0791b1aeb47106eda
Spawar1530 <spawar1530@gmail.com>
2022-04-12 13:05:17 +0530

    12 april problems

---
+++ 12_april_problems.js
@@ -0,0 +1,21 @@
+//que 1: Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order
+//Examples: minMax([1, 2, 3, 4, 5]) ➞ [1, 5], minMax([2334454, 10, 200, 5, 900]) ➞ [5, 2334454]
+
+//que 2: Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+//largestSwap(27) ➞ false, largestSwap(43) ➞ true
+//If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. On the other hand, 
+//swapping 43 gives us 34, and 43 > 34
+
+//que 3: Create a function to return the amount of potatoes there are in a string
+//Examples: potatoes("potato") ➞ 1, potatoes("potatopotato") ➞ 2
+
+//que 4: Your task is to create a Circle constructor that creates a circle with a radius provided by an argument. 
+//The circles constructed must have two methods getArea() (PI*r^2) and 
+//getPerimeter() (2*PI*r) which give both respective areas and perimeter (circumference).
+
+//Examples: let circy = new Circle(11)
+//circy.getArea()
+// Should return 380.132711084365
+//let circy = new Circle(4.44)
+//circy.getPerimeter()
+// Should return 27.897342763877365
