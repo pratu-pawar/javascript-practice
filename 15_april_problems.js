508bea054da5a55b8a3df67e3c0a0b7aff4460af
pratu-pawar <pratikshapawar3498@gmail.com>
2022-05-02 12:14:04 +0530

    object

--- 15_april_problems.js
+++ 15_april_problems.js
@@ -2,6 +2,12 @@
 //returns a string of those numbers formatted as a phone number (e.g. (555) 555-5555)
 //Examples: formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
 
+function formatPhoneNumber(array){ 
+    return `(${array.slice(0, 3).join('')}) ${array.slice(3, 6).join('')} - ${array.slice(6).join('')}`;
+
+}
+console.log(formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
+
 
 //Que 2: Create a function that takes two numbers as arguments (num, length) and 
 //returns an array of multiples of num until the array length reaches length
@@ -11,5 +17,18 @@
 //A box is represented by an array with three elements: length, width and height.
 //For instance, totalVolume([2, 3, 2], [6, 6, 7], [1, 2, 1]) should return 266 since 
 //(2 x 3 x 2) + (6 x 6 x 7) + (1 x 2 x 1) = 12 + 252 + 2 = 266.
+//totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]
+//Examples: ) ➞ 63, totalVolume([2, 2, 2], [2, 1, 1]) ➞ 10
 
-//Examples: totalVolume([4, 2, 4], [3, 3, 3], [1, 1, 2], [2, 1, 1]) ➞ 63, totalVolume([2, 2, 2], [2, 1, 1]) ➞ 10
+x =  sumAll([2, 2, 2], [2, 1, 1]);
+function sumAll(){ 
+    var i ;
+    
+    for (i = 0; i < arguments.length; i++){ 
+        sum += arguments[i];
+    }
+    return sum;
+
+}
+
+console.log(sumAll());
---
+++ object.js
@@ -0,0 +1,59 @@
+// let lecture = 10;
+// let section = 3;
+// let title = 'javascript';
+
+// const course = { 
+//     lecture: 10,
+//     section: 3,
+//     title: 'javascript',
+//     notes: {  
+//         introduction: "welcome to js course"
+//     },
+//     enroll(){ 
+//         console.log('youare sucessfully enrolled');
+//     }
+
+// }
+
+// function enroll(){ 
+//     console.log('you are sucessfully enrolled');
+// }
+
+// course.enroll()
+
+
+
+//factoury function
+// function createCource(){ 
+// return { 
+//     lecture: 10,
+//     section: 3,
+//     title: 'javascript',
+//     notes: {  
+//         introduction: "welcome to js course"
+//     },
+//     enroll(){ 
+//         console.log('you are sucessfully enrolled');
+//     }
+
+// }
+// }
+// const cource = createCource();
+// cource.enroll();
+
+
+//constructor function 
+// function Cource(tital){ 
+//     this.tital = tital,
+//     this.enroll = function(){ 
+//         console.log('you are sucessfully enrolled');
+//     }
+// }
+
+// const cource = new Cource('javascript')
+// delete cource.tital;
+// cource.checkenrollment = function(){ 
+//     console.log( '30 users are enrolled');
+// }
+// cource.enroll()
+
