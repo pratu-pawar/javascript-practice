9119c274080fcc5969885f94dd7541f8cdc18389
Spawar1530 <spawar1530@gmail.com>
2022-05-09 11:10:14 +0530

    if else problems

---
+++ conditions.js
@@ -0,0 +1,68 @@
+"For All the questions and answers visit this website: https://contactmentor.com/javascript-if-else-exercises-solutions/"
+
+// 1. Check if a number is odd or even in JavaScript
+// Function `isEvenOrOdd()` checks if input number is even or odd by using “%” operator in JavaScript.
+// Print “Number is even” if the number is divisible by 2.
+// Else print “Number is odd” if the number returns a remainder when divided by 2.
+
+// 2. Check if input variable is a number or not
+// Function `isNumber()` checks if input variable is a number by using isNaN() in-built JavaScript function. 
+// Print “Variable is not a number” if isNaN() returns true.
+// Else print “Variable is a valid number” if isNaN() returns false.
+
+// 3. Find the largest of two number
+// Function `findLargest()` finds the largest between two number by using “>” and “=” operator in JavaScript.
+// Print num1 is the largest if num1>num2.
+// Print num2 is the largest if num1<num2.
+// Else print num1 and num2 are equal when num1==num2.
+
+// 4. Find the largest of three number
+// Function `findLargest()` finds the largest of three number by using “>” and “&&” operator in JavaScript.
+// Print num1 is the largest if num1>num2 and num1>num3.
+// Print num2 is the largest if num2<num3.
+// Else print num3.
+
+// 5. Check if a triangle is equilateral, scalene, or isosceles
+// Function `findTriangleType()` finds the type of the triangle for given side values by using “==” and “&&” operator in JavaScript.
+// Print “Equilateral triangle.” if values for all side1, side2 and side3 are equal.
+// Print “Isosceles triangle.” if values for side1 is equal to side2 or side2 is equal to side3
+// Else “Scalene triangle.” since values of all sides are unequal.
+
+// 6. Find the a number is present in given range
+// Function `checkInRange()` finds if the given number is within the provided start and end range using >=, <= and && operators in JavaScript.
+// Print “Between the range” if num is between start and end values
+// Else Print “Outside the range” since num is outside start and end values.
+
+// 7. Perform arithmetic operations on two numbers
+// Function `evalNumbers()` prints the result after evaluating arithmetic operations between two numbers of 
+// addition, multiplication, division, and modulus in JavaScript.
+// Print result of num1+num2 if operation is “add”
+// Print result of num1-num2 if operation is “subtract”
+// Print result of num1*num2 if operation is “multiply”
+// Print result of num1/num2 if operation is “divide”
+// Print result of num1%num2 if operation is “modulus”
+// Else print “Invalid operation”
+
+// 8. Find check if a year is leap year or not
+// Function `checkLeapYear()` find if the given year is a leap year or not by using %, !=, && and || operators in JavaScript.
+// If year is divisble by 4 and not divisble by 100 then print “leap year”.
+// Or if year is divisible by 400 then print “leap year”.
+// Else print “not a leap year”.
+
+// 9. Find the grade for input marks
+// Function `findGrade()` to find the grade of the student based on the input marks.
+// Print “S grade” if marks is between 90 and 100.
+// Print “A grade” if marks is between 80 and 90.
+// Print “B grade” if marks is between 70 and 80.
+// Print “C grade” if marks is between 60 and 70.
+// Print “D grade” if marks is between 50 and 60.
+// Print “E grade” if marks is between 40 and 50.
+// Print “Student has failed” if marks is between 0 and 40.
+// Else print “Invalid marks”.
+
+// 10. Find number of days in a given month
+// Function `findDaysInMonth()` finds the number of days in a given month of a year.
+// If month is outside the range of 1 and 12 print “Invalid month”.
+// If month is equal to 2 ie, February print “29 days” if leap year else print “28 days” .
+// Else if month is equal to 4, 6, 9 or 11 print “30 days”.
+// Else print “31 days”
